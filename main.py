98ca0e5ca0ea85b30c0cd6ac810ff62d1d1f0182
Ryan Carvalho <115719792+RyanCarvalho3@users.noreply.github.com>
2022-11-13 21:29:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from tkinter import *
+
+
+janela = Tk()
+
+janela.title("MeuPlanner.com")
+
+janela.configure(background ="#f2e4f9")
+janela.geometry('300x500')
+janela.mainloop()
+
+
+#label
+
+caixa = Label(janela, text = "MeuPlanner.com", background='white')
+caixa.place(x=100,y=120)
+
+#Entry
+
+entrada = Entry(janela)
+entrada.place(x=100,y=120)
+
+botao = Buttom(janela, text="login")
+botao.place(x=100,y=120)
+
+janela1 = Tk()
+
+janela1.title("Login")
+janela.configure(background="#aa00ff")
+janela.geometry('200x400')
+from replit import db
+from replit import db
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+    
+
+
